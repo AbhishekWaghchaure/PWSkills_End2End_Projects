01d4a8d44f4e6717a5804e9b58ccf91f551f434b
Abhishek Waghchaure <abhisw28@gmail.com>
2023-08-18 19:48:09 +0530

    Model training done

--- Diamond_Price_Prediction/src/pipelines/training_pipeline.py
+++ Diamond_Price_Prediction/src/pipelines/training_pipeline.py
@@ -19,6 +19,7 @@ import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.data_trainer import ModelTrainer
 
 
 if __name__=='__main__':
@@ -29,3 +30,6 @@ if __name__=='__main__':
     data_transformation = DataTransformation()
     train_arr,test_arr,obj_path = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
     
+    
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
